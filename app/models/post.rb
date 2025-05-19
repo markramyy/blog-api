460da02fbbecc3c2a5e6c17861732fcf1f2d00ea
Mark Ramy <markramyy@gmail.com>
2025-05-20 01:29:08 +0300

    Add validations for title and body in Post model, implement user-specific scope, and add method to check post ownership.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :body, presence: true
+
+  scope :by_user, ->(user) { where(user: user) }
+
+  def authored_by?(user)
+    self.user_id == user.id
+  end
 end
