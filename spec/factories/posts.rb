ead81858e52fed041f975b1df582d4fceb38d806
Mark Ramy <markramyy@gmail.com>
2025-05-20 18:38:10 +0300

    Add default tag assignment in post factory to ensure posts have a tag when created

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,5 +3,8 @@ FactoryBot.define do
     title { "MyString" }
     body { "MyText" }
     user { nil }
+    after(:build) do |post|
+      post.tag_list = "test-tag" if post.tags.empty?
+    end
   end
 end
