950b4b6013655e4145d13d3125a91dbb6e7ff29a
Mark Ramy <markramyy@gmail.com>
2025-05-20 18:38:15 +0300

    Add content presence validation and authored_by? method to Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :content, presence: true
+
+  def authored_by?(user)
+    self.user_id == user.id
+  end
 end
