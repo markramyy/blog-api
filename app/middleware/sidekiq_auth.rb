2ca68d614e706d1d8f75ca456b014c0aef9169ae
Mark Ramy <markramyy@gmail.com>
2025-05-20 09:38:00 +0300

    Create Sidekiq authentication middleware to secure the web interface with basic authentication using environment variables for credentials.

---
+++ app/middleware/sidekiq_auth.rb
@@ -0,0 +1,10 @@
+module SidekiqAuth
+  class Web
+    def self.use!(username: ENV['SIDEKIQ_USERNAME'], password: ENV['SIDEKIQ_PASSWORD'])
+      Sidekiq::Web.use Rack::Auth::Basic, "Protected Area" do |user, pass|
+        ActiveSupport::SecurityUtils.secure_compare(user, username) &
+        ActiveSupport::SecurityUtils.secure_compare(pass, password)
+      end
+    end
+  end
+end
