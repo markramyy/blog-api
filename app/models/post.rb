edf458266426712c24333055bebe4e398ab8cc1c
Mark Ramy <markramyy@gmail.com>
2025-05-20 18:15:06 +0300

    Enhance title validation in Post model to enforce a maximum length of 255 characters

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true
   validate :has_at_least_one_tag
 
