f3943061ce66bea78f99f31a79770b6d87ca0986
Mark Ramy <markramyy@gmail.com>
2025-05-20 09:19:12 +0300

    Add TagsController for managing tags in the API, including index and create actions with error handling for tag creation.

---
+++ app/controllers/api/v1/tags_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class TagsController < ApplicationController
+      def index
+        @tags = Tag.all
+        render json: @tags
+      end
+
+      def create
+        @tag = Tag.new(tag_params)
+
+        if @tag.save
+          render json: @tag, status: :created
+        else
+          render json: { errors: @tag.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def tag_params
+        params.require(:tag).permit(:name)
+      end
+    end
+  end
+end
